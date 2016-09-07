15907a49e93b2461aac2a783d47aea29b3b6b698
Gabriel Kozma <gkozma@br.ibm.com>
2016-09-07 16:07:42 -0300

    starting project

---
+++ swift-mask-textfield/swift-mask-textfield/swift-mask-textfield.h
@@ -0,0 +1,19 @@
+//
+//  swift-mask-textfield.h
+//  swift-mask-textfield
+//
+//  Created by Gabriel Kozma on 9/7/16.
+//  Copyright © 2016 gabrielmackoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swift-mask-textfield.
+FOUNDATION_EXPORT double swift_mask_textfieldVersionNumber;
+
+//! Project version string for swift-mask-textfield.
+FOUNDATION_EXPORT const unsigned char swift_mask_textfieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_mask_textfield/PublicHeader.h>
+
+
