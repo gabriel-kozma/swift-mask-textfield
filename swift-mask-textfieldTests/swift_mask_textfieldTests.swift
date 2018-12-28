764031846601f64c8af39eb7128dd06882d76536
Gabriel Maccori Kozma <gabrielmackoz@gmail.com>
2018-12-28 11:58:01 -0200

    adding prefix functionality (#10)

--- swift-mask-textfieldTests/swift_mask_textfieldTests.swift
+++ swift-mask-textfieldTests/swift_mask_textfieldTests.swift
@@ -13,7 +13,7 @@ class swift_mask_textfieldTests: XCTestCase {
     
     let textField = SwiftMaskTextfield(frame: CGRect(x: 0, y: 0, width: 200, height: 20))
     
-    func testTextField_WithDigitsOnlyFormatPatternAndValidate_ShouldSucceed() {
+    func testTextField_WithDigitsOnlyFormatPattern() {
         self.measure {
             self.textField.formatPattern = "#####.#####  #####.#####"
             self.textField.text = "01234567890123456789"
@@ -22,7 +22,7 @@ class swift_mask_textfieldTests: XCTestCase {
         }
     }
 
-    func testTextField_WithLettersOnlyFormatPatternAndValidate_ShouldSucceed() {
+    func testTextField_WithLettersOnlyFormatPattern() {
         self.measure {
             self.textField.formatPattern = "@@@@@.@@@@@"
             self.textField.text = "abcdeABCDE"
@@ -31,7 +31,7 @@ class swift_mask_textfieldTests: XCTestCase {
         }
     }
     
-    func testTextField_WithLowercaseLettersOnlyFormatPatternAndValidate_ShouldSucceed() {
+    func testTextField_WithLowercaseLettersOnlyFormatPattern() {
         self.measure {
             self.textField.formatPattern = "aaaaa.aaaaa"
             self.textField.text = "abcdeABCDEabcde"
@@ -40,7 +40,7 @@ class swift_mask_textfieldTests: XCTestCase {
         }
     }
     
-    func testTextField_WithUppercaseLettersOnlyFormatPatternAndValidate_ShouldSucceed() {
+    func testTextField_WithUppercaseLettersOnlyFormatPattern() {
         self.measure {
             self.textField.formatPattern = "AAAAA.AAAAA"
             self.textField.text = "abcdeABCDEabcdeABCDE"
@@ -49,12 +49,22 @@ class swift_mask_textfieldTests: XCTestCase {
         }
     }
     
-    func testTextField_WithCustomFormatPatternAndValidate_ShouldSucceed() {
+    func testTextField_WithCustomFormatPattern() {
         self.measure {
             self.textField.formatPattern = "#Aa@*.#Aa@*"
             self.textField.text = "AfaaGA125GA1ab51c521d5e5ABFAC52FA1DEAFaAFAb521cde521ABCDE"
             
             XCTAssertEqual(self.textField.text, "1Gab5.1AaAF")
+        }
+    }
+    
+    func testTextfield_WithDigitsAndPrefix() {
+        self.measure {
+            self.textField.formatPattern = "#####-####"
+            self.textField.prefix = "+55 "
+            self.textField.text = "01234567890123456789"
+            
+            XCTAssertEqual(self.textField.text, "+55 01234-5678")
         }
     }
 }
